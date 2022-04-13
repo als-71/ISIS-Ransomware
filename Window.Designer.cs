59e84a5adcf4b11a65df1b39fce7d5d4a63ff853
fed#1234 <87880215+fedintel@users.noreply.github.com>
2022-04-13 12:20:05 -0400

    Add files via upload

---
+++ Window.Designer.cs
@@ -0,0 +1,268 @@
+﻿namespace ISIS1
+{
+    partial class ISIS
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ISIS));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.crypto = new System.Windows.Forms.LinkLabel();
+            this.ransom = new System.Windows.Forms.LinkLabel();
+            this.bitcoin = new System.Windows.Forms.LinkLabel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Red;
+            this.textBox1.Location = new System.Drawing.Point(12, 419);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(776, 226);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = resources.GetString("textBox1.Text");
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(-8, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(814, 39);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "YOUR FILES HAVE BEEN ENCRYPTED BY ISIS";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.Red;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.textBox2.Location = new System.Drawing.Point(12, 52);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(330, 26);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "1Bq8Q485FPVSwZq89vMxVhbxzoEfwNcZ52";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button1.Location = new System.Drawing.Point(349, 52);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "COPY";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.Color.Red;
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.textBox3.Location = new System.Drawing.Point(12, 84);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(330, 26);
+            this.textBox3.TabIndex = 5;
+            this.textBox3.Text = "1Bq8Q485FPVSwZq89vMxVhbxzoEfwNcZ52";
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button2.Location = new System.Drawing.Point(348, 84);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "DECRYPT";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(467, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(311, 39);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "HOW CAN I PAY?";
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.Color.Red;
+            this.textBox4.Location = new System.Drawing.Point(517, 94);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(224, 198);
+            this.textBox4.TabIndex = 8;
+            this.textBox4.Text = "All you need to do is  pay $100 worth of Bitcoin to the Address next to this box " +
+    "easy as that!\r\n\r\nBye! :)";
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // crypto
+            // 
+            this.crypto.ActiveLinkColor = System.Drawing.Color.Transparent;
+            this.crypto.AutoSize = true;
+            this.crypto.BackColor = System.Drawing.Color.Transparent;
+            this.crypto.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold);
+            this.crypto.Location = new System.Drawing.Point(27, 193);
+            this.crypto.Name = "crypto";
+            this.crypto.Size = new System.Drawing.Size(390, 25);
+            this.crypto.TabIndex = 9;
+            this.crypto.TabStop = true;
+            this.crypto.Text = "Dont have a Crypto Wallet? Click Here!";
+            // 
+            // ransom
+            // 
+            this.ransom.ActiveLinkColor = System.Drawing.Color.Transparent;
+            this.ransom.AutoSize = true;
+            this.ransom.BackColor = System.Drawing.Color.Transparent;
+            this.ransom.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold);
+            this.ransom.Location = new System.Drawing.Point(27, 259);
+            this.ransom.Name = "ransom";
+            this.ransom.Size = new System.Drawing.Size(227, 25);
+            this.ransom.TabIndex = 10;
+            this.ransom.TabStop = true;
+            this.ransom.Text = "What is Ransomware?";
+            // 
+            // bitcoin
+            // 
+            this.bitcoin.ActiveLinkColor = System.Drawing.Color.Transparent;
+            this.bitcoin.AutoSize = true;
+            this.bitcoin.BackColor = System.Drawing.Color.Transparent;
+            this.bitcoin.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold);
+            this.bitcoin.Location = new System.Drawing.Point(27, 141);
+            this.bitcoin.Name = "bitcoin";
+            this.bitcoin.Size = new System.Drawing.Size(168, 25);
+            this.bitcoin.TabIndex = 11;
+            this.bitcoin.TabStop = true;
+            this.bitcoin.Text = "What is Bitcoin?";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Bold);
+            this.label3.Location = new System.Drawing.Point(284, 259);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(188, 39);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "TIME OUT";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Bold);
+            this.label4.Location = new System.Drawing.Point(302, 336);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(157, 39);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "00:00:00";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Bold);
+            this.label5.Location = new System.Drawing.Point(214, 375);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(501, 39);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "dddd, mm ddd yyyy, hh mm ss";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // ISIS
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.BackgroundImage = global::ISIS1.Properties.Resources.isis;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 657);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.bitcoin);
+            this.Controls.Add(this.ransom);
+            this.Controls.Add(this.crypto);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "ISIS";
+            this.Text = "ISIS";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Window_FormClosing);
+            this.Load += new System.EventHandler(this.ISIS_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.LinkLabel crypto;
+        private System.Windows.Forms.LinkLabel ransom;
+        private System.Windows.Forms.LinkLabel bitcoin;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
